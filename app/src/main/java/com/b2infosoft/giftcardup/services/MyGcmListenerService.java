3b1cfd887a1fe96de5065cd825a21e1f98a3c5e2
b2infosoftandroid <rajesh@b2infosoft.in>
2016-09-20 17:35:55 +0530

    payal

--- app/src/main/java/com/b2infosoft/giftcardup/services/MyGcmListenerService.java
+++ app/src/main/java/com/b2infosoft/giftcardup/services/MyGcmListenerService.java
@@ -1,7 +1,16 @@
 package com.b2infosoft.giftcardup.services;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 
+import com.b2infosoft.giftcardup.R;
+import com.b2infosoft.giftcardup.activity.Main;
 import com.google.android.gms.gcm.GcmListenerService;
 
 /**
@@ -19,7 +28,23 @@ public class MyGcmListenerService extends GcmListenerService {
     }
 
     private void sendNotification(String msg){
+        Intent intent = new Intent(this, Main.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
 
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle("GCM Message")
+                .setContentText(msg)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
 
     }
 }
