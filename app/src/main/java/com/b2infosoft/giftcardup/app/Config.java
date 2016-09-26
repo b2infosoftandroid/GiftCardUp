5b7aa67331e7ff82f402f8be4e15f8f19609302b
b2infosoftandroid <rajesh@b2infosoft.in>
2016-09-26 09:42:45 +0530

    payal

--- app/src/main/java/com/b2infosoft/giftcardup/app/Config.java
+++ app/src/main/java/com/b2infosoft/giftcardup/app/Config.java
@@ -4,7 +4,7 @@ package com.b2infosoft.giftcardup.app;
  * Created by rajesh on 7/24/2016.
  */
 public class Config {
-    private final String SERVER_IP = "192.168.0.59";
+    private final String SERVER_IP = "192.168.0.52";
     public final String GIFT_CARD_IMAGE_FOLDER ="images/upload/";
     public final String USER_PROFILE_IMAGE_FOLDER ="img/avatar/";
     private final String REFERRAL_ADDRESS = "http://giftcardup.com/signup.php?ref=";
